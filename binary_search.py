be74aa385e842fe637b08c980bce81315a97c6a5
Incheol-Shine <parpedian@gmail.com>
2021-11-05 23:29:13 +0900

    binary_serch BOJ17609 2nd solution

--- binary_search.py
+++ binary_search.py
@@ -63,4 +63,33 @@
     #     print(f(i,j,flag))
 # BOJ17609()
 
+# def BOJ17609_2(): # 회문 2nd solution
+    # def is_palindrome(s):
+    #     if s == s[::-1]:
+    #         return True
+
+    # def solution(s):
+    #     if is_palindrome(s):
+    #         return 0
+        
+    #     i = 0
+    #     j = len(s) - 1
+
+    #     while i < j:
+    #         if s[i] != s[j]:
+    #             if is_palindrome(s[i + 1: j + 1]):
+    #                 return 1
+    #             elif is_palindrome(s[i: j]):
+    #                 return 1
+    #             else:
+    #                 return 2
+    #         i += 1
+    #         j -= 1
+
+    # n = int(input())
+    # for _ in range(n):
+    #     s = input().strip()
+    #     print(solution(s))
+# BOJ17609_2()
+
 ###
